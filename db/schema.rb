b0018aacb2f42d954d965639a9afeec401092fcd
stewartimel <stewartimel@gmail.com>
2014-01-11 13:16:43 -0600

    Move footer to application layout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140110213905) do
+ActiveRecord::Schema.define(version: 20140111190850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20140110213905) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "attachment"
   end
 
   add_index "sitters", ["email"], name: "index_sitters_on_email", unique: true, using: :btree
