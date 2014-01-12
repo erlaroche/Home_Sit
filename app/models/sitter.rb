e0ca2443d57ed6b7db4dc656df8751885e688654
stewartimel <stewartimel@gmail.com>
2014-01-12 16:10:47 -0600

    Configure devise lockable

--- app/models/sitter.rb
+++ app/models/sitter.rb
@@ -2,7 +2,7 @@ class Sitter < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :lockable
 
   has_many :owners, :through => :appointments
   has_many :appointments, :through => :owners
