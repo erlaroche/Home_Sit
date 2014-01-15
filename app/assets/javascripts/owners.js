9bbf69b07e8fbea46996811e94343713a2ca9424
MakerSquare Student <mzseidman@gmail.com>
2014-01-15 15:48:40 -0600

    add parallax

--- app/assets/javascripts/owners.js
+++ app/assets/javascripts/owners.js
@@ -13,3 +13,17 @@ $('#findsitterbutton').on('click', function () {
 $('#findsitterbuttonagain').on('click', function () {
   window.location = '/sitters/index'
 });
+
+
+$('.bgParallax').each(function(){
+    var $obj = $(this);
+ 
+    $(window).scroll(function() {
+        var yPos = -($(window).scrollTop() / $obj.data('speed'));
+ 
+        var bgpos = '50% '+ yPos + 'px';
+ 
+        $obj.css('background-position', bgpos );
+  
+    });
+});
