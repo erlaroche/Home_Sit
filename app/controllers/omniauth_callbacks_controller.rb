f6416904a6b408bdc1f1630b11135695b5393656
stewartimel <stewartimel@gmail.com>
2014-01-15 15:18:09 -0600

    add comments to sitters controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,9 +1,15 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def all
+    # Get Authorization code from Google
     code = request.original_url[params[:code]]
+    
+    # Get authorization hash from Google
     auth_hash = request.env["omniauth.auth"]
+    
+    # Call from_omniauth method from Sitter model
     sitter = Sitter.from_omniauth(auth_hash, code)
+    
     if sitter.persisted?
       sign_in_and_redirect sitter, notice: "Signed in!"
     else
