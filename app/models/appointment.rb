2064d4e782918a40a9970d4a2655c7391b146d68
stewartimel <stewartimel@gmail.com>
2014-01-19 18:28:24 -0600

    More time convert fixes

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,26 +4,34 @@ class Appointment < ActiveRecord::Base
   belongs_to :sitter
 
   def time_start_convert
-    if self.time_start.slice(-2, 2) == "PM" && self.time_start.slice(0,2) != "12"
-      new_time = self.time_start.partition(":")[0].to_i + 12 
-      time = "#{self.date}" + "T" + "#{new_time}" + ":00:00" + "Z"
+    if self.time_start.slice(-2, 2) == "PM"
+      if self.time_start.slice(0,2) == "12"
+        new_time = "12" 
+      else
+        new_time = self.time_start.partition(":")[0].to_i + 12 
+      end
+      "#{self.date}" + "T" + "#{new_time}" + ":00:00" + "Z"
     else
       new_time = self.time_start.partition(":")[0]
       new_time = "00" if new_time == "12"
       new_time = "0" + "#{new_time}" if new_time.length == 1
-      time = "#{self.date}" + "T" + "#{new_time}" + ":00:00" + "Z"
+      "#{self.date}" + "T" + "#{new_time}" + ":00:00" + "Z"
     end
   end
 
   def time_end_convert
-    if self.time_end.slice(-2, 2) == "PM" && self.time_end.slice(0,2) != "12"
-      new_time = self.time_end.partition(":")[0].to_i + 12 
-      time = "#{self.date}" + "T" + "#{new_time}" + ":00:00" + "Z"
+    if self.time_end.slice(-2, 2) == "PM"
+      if self.time_end.slice(0,2) == "12"
+        new_time = "12" 
+      else
+        new_time = self.time_end.partition(":")[0].to_i + 12 
+      end
+      "#{self.date}" + "T" + "#{new_time}" + ":00:00" + "Z"
     else
       new_time = self.time_end.partition(":")[0]
       new_time = "00" if new_time == "12"
       new_time = "0" + "#{new_time}" if new_time.length == 1
-      time = "#{self.date}" + "T" + "#{new_time}" + ":00:00" + "Z"
+      "#{self.date}" + "T" + "#{new_time}" + ":00:00" + "Z"
     end
   end
 
