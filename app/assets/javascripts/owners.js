a9e193ee3410be20c0f417403c479ef4192d9e68
erlaroche <E.R.Laroche@gmail.com>
2014-01-21 17:25:04 -0600

    Add alterations to homepage

--- app/assets/javascripts/owners.js
+++ app/assets/javascripts/owners.js
@@ -9,28 +9,9 @@ $('#sitterbutton').on('click', function () {
   window.location = '/sitters/auth/google_oauth2'
 });
 
-
-//////////////mouseover?????????????????
-
-
-$("#findsitterbutton").click(function() {
-    doBounce($(this), 3, '10px', 300);   
+$('#findsitterbutton').on('click', function () {
+  window.location = '/appointments/new'
 });
-
-
-function doBounce(element, times, distance, speed) {
-    for(i = 0; i < times; i++) {
-        element.animate({marginTop: '-='+distance},speed)
-            .animate({marginTop: '+='+distance},speed);
-    }  
-    $('#findsitterbutton').on('click', function () {
-      window.location = '/appointments/new'
-    });      
-}
-
-
-
-
 
 $('#findsitterbuttonagain2').on('click', function () {
   window.location = '/appointments'
@@ -39,6 +20,33 @@ $('#findsitterbuttonagain2').on('click', function () {
 $('#findsitterbuttonagain').on('click', function () {
   window.location = '/sitters/index'
 });
+
+
+//////////////mouseover?????????????????
+
+
+// $("#findsitterbutton").click(function() {
+//     doBounce($(this), 3, '10px', 300);   
+// });
+
+
+// function doBounce(element, times, distance, speed) {
+//     for(i = 0; i < times; i++) {
+//         element.animate({marginTop: '-='+distance},speed)
+//             .animate({marginTop: '+='+distance},speed);
+//     }  
+//     $('#findsitterbutton').on('click', function () {
+//       window.location = '/appointments/new'
+//     });      
+// }
+
+
+
+
+
+
+
+
 
 ////////////////////
 // Parralax stuff: //
