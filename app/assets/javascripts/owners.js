44c398563955e9d797d181192cb102ab9b4cdba6
erlaroche <E.R.Laroche@gmail.com>
2014-01-21 18:36:22 -0600

    Add buttons that bounce when hover and redirect when click

--- app/assets/javascripts/owners.js
+++ app/assets/javascripts/owners.js
@@ -5,17 +5,83 @@
 
 
 
+
+// buttons bounce on hover and redirect on click
+//first button
+$("#sitterbutton").hover(function(){
+  doBounce($(this), 2, '10px', 300);
+});
+
+function doBounce(element, times, distance, speed){
+  for(i = 0; i < times; i++) {
+      element.animate({marginTop: '-='+distance},speed)
+          .animate({marginTop: '+='+distance},speed);
+  } 
+  // $('#sitterbutton').on('click', function () {
+  //   window.location = '/sitters/auth/google_oauth2'
+  // });
+}
+
 $('#sitterbutton').on('click', function () {
   window.location = '/sitters/auth/google_oauth2'
 });
+
+// second button
+$("#findsitterbutton").hover(function() {
+    doBounce($(this), 2, '10px', 300);   
+});
+
+
+function doBounce(element, times, distance, speed) {
+    for(i = 0; i < times; i++) {
+        element.animate({marginTop: '-='+distance},speed)
+            .animate({marginTop: '+='+distance},speed);
+    }  
+    // $('#findsitterbutton').on('click', function () {
+    //   window.location = '/appointments/new'
+    // });      
+}
 
 $('#findsitterbutton').on('click', function () {
   window.location = '/appointments/new'
 });
 
+// third button
+$("#findsitterbuttonagain2").hover(function() {
+    doBounce($(this), 2, '10px', 300);   
+});
+
+
+function doBounce(element, times, distance, speed) {
+    for(i = 0; i < times; i++) {
+        element.animate({marginTop: '-='+distance},speed)
+            .animate({marginTop: '+='+distance},speed);
+    }  
+  //   $('#findsitterbuttonagain2').on('click', function () {
+  //     window.location = '/appointments'
+  // });     
+}
+
 $('#findsitterbuttonagain2').on('click', function () {
   window.location = '/appointments'
 });
+
+
+// fourth button
+$("#findsitterbuttonagain").hover(function() {
+    doBounce($(this), 2, '10px', 300);   
+});
+
+
+function doBounce(element, times, distance, speed) {
+    for(i = 0; i < times; i++) {
+        element.animate({marginTop: '-='+distance},speed)
+            .animate({marginTop: '+='+distance},speed);
+    }  
+ // $('#findsitterbuttonagain').on('click', function () {
+ //   window.location = '/sitters/index'
+ // });    
+}
 
 $('#findsitterbuttonagain').on('click', function () {
   window.location = '/sitters/index'
@@ -25,20 +91,7 @@ $('#findsitterbuttonagain').on('click', function () {
 //////////////mouseover?????????????????
 
 
-// $("#findsitterbutton").click(function() {
-//     doBounce($(this), 3, '10px', 300);   
-// });
 
-
-// function doBounce(element, times, distance, speed) {
-//     for(i = 0; i < times; i++) {
-//         element.animate({marginTop: '-='+distance},speed)
-//             .animate({marginTop: '+='+distance},speed);
-//     }  
-//     $('#findsitterbutton').on('click', function () {
-//       window.location = '/appointments/new'
-//     });      
-// }
 
 
 
