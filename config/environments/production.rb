bda73fd8e0beffa32260939a1fa4e1454d3babb2
stewartimel <stewartimel@gmail.com>
2014-01-28 11:59:58 -0600

    email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ HouseSitter::Application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'herokuapp' }
+  # config.action_mailer.default_url_options = { :host => 'herokuapp' }
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
@@ -80,7 +80,7 @@ HouseSitter::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'http://sit4u.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'herokuapp' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
